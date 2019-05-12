16bdb5d36430fb76e267dfe6c3303f4b9a813f43
c-r-5 <48353236+c-r-5@users.noreply.github.com>
2019-05-12 20:11:15 +0900

    sugar4py(大嘘)

---
+++ sugar4py.py
@@ -0,0 +1,62 @@
+DOMAIN = 'domain'
+BOOL = 'bool'
+INT = 'int'
+
+ABS = 'abs'
+NEG = 'neg'
+ADD = '+'
+SUB = '-'
+MUL = '*'
+DIV = '/'
+MOD = '%'
+POW = 'pow'
+MIN = 'min'
+MAX = 'max'
+IF = 'if'
+
+RELATION = 'relation'
+SUPPORTS = 'supports'
+CONFLICTS = 'conflicts'
+
+PREDICATE = 'predicate'
+
+NOT = 'not'
+AND = 'and'
+OR = 'or'
+XOR = 'xor'
+IMP = 'imp'
+IFF = 'iff'
+
+FALSE = 'false'
+TRUE = 'true'
+LE = '<='
+LT = '<'
+GE = '>='
+GT = '>'
+EQ = '='
+NE = '!='
+
+NIL = 'nil'
+
+ALLDIFFERENT = 'alldifferent'
+WEIGHTEDSUM = 'weightedsum'
+CUMULATIVE = 'cumulative'
+ELEMENT = 'element'
+DISJUNCTIVE = 'disjunctive'
+LEX_LESS = 'lex_less'
+LEX_LESSEQ = 'lex_lesseq'
+NVALUE = 'nvalue'
+GLOBAL_CARDINALITY = 'global_cardinality'
+GLOBAL_CARDINALITY_WITH_COSTS = 'global_cardinality_with_costs'
+COUNT = 'count'
+OBJECTIVE, MINIMIZE, MAXIMIZE = 'objective', 'minimize', 'maximize'
+
+
+def create(*s):
+    if len(s)==1:
+        return str(s[0])
+    return '({})'.format(' '.join(map(str,s)))
+
+
+def add(*s):
+    print(create(*s))

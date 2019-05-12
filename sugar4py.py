817e09374eb04e3c00bb49392f7bd35a7d71fb2e
c-r-5 <48353236+c-r-5@users.noreply.github.com>
2019-05-12 20:39:55 +0900

    Update sugar4py.py

--- sugar4py.py
+++ sugar4py.py
@@ -53,9 +53,9 @@ OBJECTIVE, MINIMIZE, MAXIMIZE = 'objective', 'minimize', 'maximize'
 
 
 def create(*s):
-    if len(s)==1:
+    if len(s) == 1:
         return str(s[0])
-    return '({})'.format(' '.join(map(str,s)))
+    return '({})'.format(' '.join(map(str, s)))
 
 
 def add(*s):
